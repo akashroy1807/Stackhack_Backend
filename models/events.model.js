6d2eedf63ec70198274165a6406476aed42a0f96
Aishik Deb <debaishik14@gmail.com>
2020-06-05 02:37:31 +0530

    event and register update

--- models/events.model.js
+++ models/events.model.js
@@ -5,12 +5,16 @@ const Schema = mongoose.Schema;
 const eventSchema = new Schema({
     eventId: { type: Number, required: true, unique: true },
     eventName: { type: String, required: true },
+    eventSummary: { type: String, required: true },
     eventDescription: { type: String, required: true },
-    eventDate: {type: Date, required: true},
+    eventStartDate: {type: Date, required: true},
+    eventEndDate: {type: Date, required: true},
     eventParticipants: {type: Number, required: true},
+    eventMaxParticipants: {type: Number, required: true},
     eventOwner: { type: String, required: true},
     ticketPrice: {type: Number, required: true},
-    eventPic: {type: String}
+    eventPic: {type: String},
+    status : {type: String, required: true}
 },
 {
     timestamps: true
--- routes/events.js
+++ routes/events.js
@@ -2,25 +2,29 @@ const router = require('express').Router();
 let Event = require('../models/events.model');
 
 router.route('/').get((req,res) => {
-    Event.find()
+    Event.find({status: "active"},'eventId eventName eventSummary ticketPrice eventPic eventStartDate eventEndDate')
         .then(events => res.json(events))
         .catch(err => res.status(400).json('Error:' + err));
 });
 
 router.route('/:id').get((req,res) => {
-    Event.findById(req.params.id)
+    Event.findOne({eventId: req.params.id})
         .then(events => res.json(events))
         .catch(err => res.status(400).json('Error:' + err));
 });
 
 router.route('/add').post((req,res) => {
     const eventName = req.body.eventName;
+    const eventSummary = req.body.eventSummary;
     const eventDescription = req.body.eventDescription;
-    const eventDate = Date.parse(req.body.eventDate);
-    const eventParticipants = req.body.eventParticipants;
+    const eventStartDate = Date.parse(req.body.eventStartDate);
+    const eventEndDate = Date.parse(req.body.eventEndDate);
+    const eventParticipants = 0;
+    const eventMaxParticipants = req.body.eventMaxParticipants;
     const eventOwner = req.body.eventOwner;
     const ticketPrice = req.body.ticketPrice;
     const eventPic = req.body.eventPic;
+    const status = "active";
     eventId = 0;
     Event.findOne().sort({createdAt: -1})
         .then(last => {
@@ -29,7 +33,7 @@ router.route('/add').post((req,res) => {
             } else {
                 eventId = last.eventId + 1;
             }
-            const newEvent = new Event({eventId, eventName, eventDescription, eventDate, eventParticipants, eventOwner, ticketPrice, eventPic});
+            const newEvent = new Event({eventId, eventName, eventSummary, eventDescription, eventStartDate, eventEndDate, eventParticipants, eventMaxParticipants, eventOwner, ticketPrice, eventPic, status});
             newEvent.save()
                 .then(() => res.json('Event Saved in Database'))
                 .catch(err => res.status(400).json('Error:' + err));          
@@ -42,12 +46,15 @@ router.route('/edit').post((req,res) => {
     Event.findById(req.body.id)
     .then(event => {
         event.eventName = req.body.eventName;
+        event.eventSummary = req.body.eventSummary;
         event.eventDescription = req.body.eventDescription;
-        event.eventDate = Date.parse(req.body.eventDate);
-        event.eventParticipants = req.body.eventParticipants;
+        event.eventStartDate = Date.parse(req.body.eventStartDate);
+        event.eventEndDate = Date.parse(req.body.eventEndDate);
+        event.eventMaxParticipants = req.body.eventMaxParticipants;
         event.eventOwner = req.body.eventOwner;
         event.ticketPrice = req.body.ticketPrice;
         event.eventPic = req.body.eventPic;
+        event.status = req.body.status;
         event.save()
             .then(() => res.json('Event updated'))
             .catch(err => res.status(400).json('Error:' + err));
@@ -56,8 +63,13 @@ router.route('/edit').post((req,res) => {
 });
 
 router.route('/delete').post((req,res) => {
-    Event.findByIdAndDelete(req.body.id)
-    .then(() => res.json('Event deleted'))
+    Event.findOne({eventId: req.body.id})
+    .then(event => {
+        event.status = "disabled";
+        event.save()
+            .then(() => res.json('Event deleted'))
+            .catch(err => res.status(400).json('Error:' + err));
+    })
     .catch(err => res.status(400).json('Error:' + err));
 });
 
