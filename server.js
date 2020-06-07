f5c9ddf160cde9cbcb2e7d468e16b2938a4be442
Aishik Deb <debaishik14@gmail.com>
2020-06-07 15:41:45 +0530

    added basic auth

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const basicAuth = require('express-basic-auth');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.port || 5000;
+const startTime = new Date();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(basicAuth({
+    users: { 'admin': 'mindwebs' }
+}))
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
@@ -32,6 +37,10 @@ app.use('/events',eventRouter);
 app.use('/register',registerRouter);
 app.use('/ticket',ticketRouter);
 app.use('/admin',adminRouter);
+
+app.route('/').get((req,res) => {
+    res.json("Server started successfully on " + startTime);
+})
 
 app.listen(port, () => {
     console.log('Server is running on port:', port);
